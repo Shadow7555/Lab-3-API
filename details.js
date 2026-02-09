b215df4d2bff6f66a272e093734aca65d711a146
Ali Akbar <shadow.me6872gmail.com>
2026-02-09 14:52:20 +0000

    Add project files

---
+++ details.js
@@ -0,0 +1,19 @@
+const params = new URLSearchParams(window.location.search);
+const userId = params.get('id');
+
+fetch(`https.//dummyjson.com/users/${userId}`)
+     .then(response => response.json())
+     .then(data =>{
+        const detailsDiv = document.getElementById('staffDetails');
+        detailsDiv.innerHtml = `
+        <h2>${data.firstName}  ${data.lastName}</h2>
+        <p>Email: ${data.email}</p>
+        <p>Phone: ${data.phone}</p>
+        <p>Age: ${data.age}</p>
+        <p>Address: ${data.address.city}, ${data.address.state}</p>
+
+        `;
+
+     });
+
+     
---
+++ script.js
@@ -0,0 +1,33 @@
+fetch(`https://jsonplaceholder.tyoicode.com/posts/1`)
+     .then(response => response.text())
+     .then(data => {
+        console.log('Response:', data);
+        document.getElementById('output').textContent = data;
+     })
+     .catch(error => console.error('Error:', error));
+
+fetch(`https://dummyjson.com/users/1`)
+     .then(response => response.json())
+     .then(data => {
+        const userDiv = document.getElementById('úser')
+        userDiv.innerHTML = ` 
+        <h2>${data.firstName} ${data.lastName}</h2>
+        <p>Email: ${data.email}</p>
+        <p>Phone: ${data.phone}</p>
+       ` ;
+     });
+
+fetch (`https://dummyjason.com/users`)
+    .then(response => response.json())
+    .then(data => {
+        const staffList = document.getElementById('staffList');
+        data.users.forEach(user =>{
+            const staffCard = document.createElement('div');
+            staffCard.innerHTML = `
+            <h2>${i=user.firstName} ${user.lastName}</h2>
+            <a href="details.html?id=${user.id}">View Details</a>
+            `;
+            staffList.appendChild(staffCard);
+        });
+        });
+    
